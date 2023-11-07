1c0485a05a9a2170d2db80089e07663238663241
Huseyn Sharifzade <huseynsharif@gmail.com>
2023-11-07 22:34:58 +0400

    Add new quiz

--- src/pages/AddQuiz.jsx
+++ src/pages/AddQuiz.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
+
 
 export default function AddQuiz() {
   return (
-    <div>AddQuiz</div>
+    <Container style={{ display: "flex", justifyContent: "center" }}>
+
+      <Form>
+        <Form.Input fluid label='Name' placeholder='Name' />
+        <Form.TextArea label='Description (optional)' placeholder='Write about the quiz...' />
+        <Form.Button primary>Create</Form.Button>
+      </Form>
+    </Container>
   )
 }
